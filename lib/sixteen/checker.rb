d438be4fd4b68808c64d59dd8d23aa3ef008ed69
Manabu Niseki <manabu.niseki@gmail.com>
2019-01-24 07:29:54 +0900

    add .config check
    add ./.config check

--- lib/sixteen/checker.rb
+++ lib/sixteen/checker.rb
@@ -7,11 +7,33 @@ module Sixteen
   class Checker
     SIXTEEN_SHOP_ENDPOINT = "http://16shop.online/api/setting/get_setting.php"
 
-    def check(domain)
+    def check_setting(domain)
       res = HTTP.post(SIXTEEN_SHOP_ENDPOINT, form: { domain: domain })
-      return {} if res.status != 200
+      return nil if res.code != 200
 
-      JSON.parse(res.body.to_s)
+      json = JSON.parse(res.body.to_s)
+      json.dig("email_result") ? json : nil
+    end
+
+    def check_config(domain)
+      %w(http https).each do |scheme|
+        url = "#{scheme}://#{domain}/.config"
+        res = HTTP.get(url)
+        next if res.code != 200
+
+        body = res.body.to_s
+        return body if body.include?("lock_platform") && body.include?("site_password")
+      rescue StandardError => _
+        next
+      end
+      nil
+    end
+
+    def check(domain)
+      setting = check_setting(domain)
+      return setting if setting
+
+      check_config(domain)
     end
 
     def self.check(domain)
--- spec/checker_spec.rb
+++ spec/checker_spec.rb
@@ -6,14 +6,14 @@ RSpec.describe Sixteen::Checker, :vcr do
   describe "#check" do
     context "when given a valid domain" do
       it "should return Hash with values" do
-        results = subject.check("movel-appleid.com")
-        expect(results).to be_an(Hash)
+        results = subject.check("aple.supportaccounts.websrcauthsummary.com")
+        expect(results).to be_a(Hash)
       end
     end
     context "when given an invalid domain" do
       it "should return Hash with nil values" do
-        results = subject.check("movelappleid.com")
-        expect(results).to be_an(Hash)
+        results = subject.check("apple-appleidd.ddns.net")
+        expect(results).to eq(nil)
       end
     end
   end
